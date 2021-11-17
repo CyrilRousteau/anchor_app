bd739ca9162ac6800730d27d0b7f9eaffdc74c61
rousteau <cyrilrousteau@gmail.com>
2021-11-18 10:11:56 +1100

    Add a splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'anchor.dart';
 import 'anchor_list_screen.dart';
@@ -21,7 +23,55 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
         primarySwatch: Colors.blue,
     ),
-    home: AnchorListScreen()
+    home: const SplashScreen()
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => const AnchorListScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            ClipRRect(
+            borderRadius: BorderRadius.circular(60),
+            child : Image.asset(
+              'assets/images/splash_screen.jpg',
+              height: 120,
+            ),
+            ),
+            // logo here
+
+            const SizedBox(
+              height: 20,
+            ),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
